f2e13dbdaba0db308742a0ed3351ee4d8c237217
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2017-12-31 00:42:35 -0800

    added smooth-scroll implementation

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
+// smooth scroll implementation
+import SmoothScroll from 'smooth-scroll';
+const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 700,
+    easing: 'easeInOutCubic'
+});
+
 // element selectors
 const body = document.querySelector('body');
 const header = document.querySelector('header');
@@ -5,16 +12,17 @@ const navbar = document.querySelector('.navbar-container');
 
 // getting initial values for element positions
 const originalNavbarTop = navbar.offsetTop;
-const headerBottom = header.getBoundingClientRect().bottom;
+const headerBottom = 
+    header.getBoundingClientRect().bottom - body.getBoundingClientRect().y;
 
+// fixes navbar after title, adds background shadow after header
 function grabNavbar() {
-    // recalculate distance from top of viewport on scroll
     const navbarRect = navbar.getBoundingClientRect();
     const bodyRect = body.getBoundingClientRect();
-    // grabs navbar at top
+
     (bodyRect.y + originalNavbarTop <= 0) ? 
         navbar.classList.add('fixed') : navbar.classList.remove('fixed');
-    // adds navbar background after header
+
     (headerBottom + bodyRect.y <= 0) ?
         navbar.classList.add('nav-shadow') : navbar.classList.remove('nav-shadow');
 }
