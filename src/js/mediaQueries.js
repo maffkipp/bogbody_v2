745d4085cf583711f6e0e5af699c53e831f59c87
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2018-01-11 15:18:52 -0600

    refactored and added third music player option in mediaQueries.js

--- src/js/mediaQueries.js
+++ src/js/mediaQueries.js
@@ -1,18 +1,26 @@
 (() => {
     const bandcamp = document.querySelector('.bandcamp-embed');
 
-    function setBandcampType() {
-        if (window.innerWidth <= 800) {
-            if (bandcamp.getAttribute('src') !== 'https://bandcamp.com/EmbeddedPlayer/album=1684609420/size=small/bgcol=ffffff/linkcol=0687f5/transparent=true/') {
-                bandcamp.setAttribute('src', 'https://bandcamp.com/EmbeddedPlayer/album=1684609420/size=small/bgcol=ffffff/linkcol=0687f5/transparent=true/');
-            }
-        } else {
-            if (bandcamp.getAttribute('src') !== 'https://bandcamp.com/EmbeddedPlayer/album=1684609420/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/') {
-                bandcamp.setAttribute('src', 'https://bandcamp.com/EmbeddedPlayer/album=1684609420/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/');
-            }
+    // checks if url matches and changes src attribute if not
+    function changeSrcAttribute(url, target) {
+        if (target.getAttribute('src') !== url) {
+            target.setAttribute('src', url);
         }
     }
 
+    // calls changeSrcAttribute for various screen widths to change bandcamp player type
+    function setBandcampType() {
+        if (window.innerWidth <= 600) {
+            changeSrcAttribute('https://bandcamp.com/EmbeddedPlayer/album=1684609420/size=small/bgcol=ffffff/linkcol=0687f5/transparent=true/', bandcamp);
+        } else if (window.innerWidth <= 800) {
+            changeSrcAttribute('https://bandcamp.com/EmbeddedPlayer/album=1684609420/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/', bandcamp);
+        } else {
+            changeSrcAttribute('https://bandcamp.com/EmbeddedPlayer/album=1684609420/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/', bandcamp);
+        }
+    }
+
+    // calls function on resize and page load
     window.addEventListener('resize', setBandcampType);
     window.onload(setBandcampType());
 })();
+
